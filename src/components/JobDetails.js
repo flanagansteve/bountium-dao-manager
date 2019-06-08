59b9172894913d89e73607464a51ef091f8b772b
Steve Flanagan <flanagan.s@husky.neu.edu>
2019-06-08 09:44:53 -0400

    really basic job details component working, renders high level details as h1 and the description as raw html

--- src/components/JobDetails.js
+++ src/components/JobDetails.js
@@ -1,6 +1,16 @@
 import React from 'react'
 
-export default class JobDetails extends React.Component {
+const JobDetails = ({jobObj}) =>
+  <div>
+    <h1>{jobObj.id}</h1>
+    <h1>{jobObj.type}</h1>
+    <h1>{jobObj.url}</h1>
+    <h1>{jobObj.created_at}</h1>
+    <h1>{jobObj.company}</h1>
+    <h1>{jobObj.company_url}</h1>
+    <h1>{jobObj.location}</h1>
+    <h1>{jobObj.title}</h1>
+    <div dangerouslySetInnerHTML={{__html: jobObj.description}} />
+  </div>
 
-
-}
+export default JobDetails
