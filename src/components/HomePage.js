00a7a7e2dd68bce2b0022b948289e0c03024c03f
Steven Arditti <steven.arditti@gmail.com>
2019-06-09 22:50:13 +0100

    Passing values to JobSearchList

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,8 +4,14 @@ import {BrowserRouter as Router, Link, Route, Switch}
     from "react-router-dom";
 
 export default class HomePage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: "PYTHON"}
+    }
 
-
+    updateCategory(value) {
+        this.state = {value: value};
+    }
 
     render() {
         return (
@@ -18,33 +24,36 @@ export default class HomePage extends React.Component {
                             <h1>Post Job</h1>
                             <div className="card">
                                 <h2>Category</h2>
-                                <select id="jobCategory">
+                                <select id="jobCategory"
+                                        onChange={() => this.updateCategory(document.getElementById("jobCategory").value)}>
                                     <option value="PYTHON">Python</option>
                                     <option value="JAVA">Java</option>
-                                    <option value="HTMLCSS">Html/CSS</option>
+                                    <option value="HTML">Html/CSS</option>
+                                    <option value="SQL">SQL</option>
                                 </select>
                                 <br/>
                                 <Link to="/job-search-list">See jobs in this category</Link>
                                 <br/>
                                 <h2>Job Title</h2>
-                                <input className="form-control" placeholder="Full Stack Developer">
+                                <input className="form-control" placeholder="Junior Python Developer Intern">
                                 </input>
                                 <br/>
                                 <h2>Job Description</h2>
-                                <textarea className="form-control">
+                                <textarea className="form-control"
+                                          placeholder="Coffee intern needed to ensure execs remain thoroughly caffeinated.">
                                 </textarea>
                                 <br/>
-                                <button className="btn btn-primary">Post</button>
+                                <button className="btn btn-primary"
+                                        onClick={() => console.log(this.state.value)}
+                                        >Post</button>
                             </div>
                         </div>
                     </div>
-
                 </div>
                 <Switch>
                     <Route path="/job-search-list"
                            render={() =>
-                               <JobSearchList keywords="python"/>
-                               }/>
+                               <JobSearchList keywords={this.state.value}/>}/>
                 </Switch>
             </Router>
         )
