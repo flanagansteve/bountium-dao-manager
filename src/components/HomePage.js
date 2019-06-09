ddbc228b4dea805a215d5377fcaf6ccbc9937781
Steven Arditti <steven.arditti@gmail.com>
2019-06-09 21:42:34 +0100

    Home page template

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -13,8 +13,29 @@ export default class HomePage extends React.Component {
                 <div className="container text-center">
                     <h1>Welcome to Bountium </h1>
                     <div className="row">
-                        <div className="col-6">
-                            <Link to="/job-search-list"> JobSearch</Link>
+                        <div className="col-12 text-left">
+                            <br/>
+                            <h1>Post Job</h1>
+                            <div className="card">
+                                <h2>Category</h2>
+                                <select id="jobCategory">
+                                    <option value="PYTHON">Python</option>
+                                    <option value="JAVA">Java</option>
+                                    <option value="HTMLCSS">Html/CSS</option>
+                                </select>
+                                <br/>
+                                <Link to="/job-search-list">See jobs in this category</Link>
+                                <br/>
+                                <h2>Job Title</h2>
+                                <input className="form-control" placeholder="Full Stack Developer">
+                                </input>
+                                <br/>
+                                <h2>Job Description</h2>
+                                <textarea className="form-control">
+                                </textarea>
+                                <br/>
+                                <button className="btn btn-primary">Post</button>
+                            </div>
                         </div>
                     </div>
 
