e48d0b372ad5c929949e2e30b4421e0111e59046
mlanotte1998 <mlanotte1998@gmail.com>
2019-06-16 22:28:08 -0400

    added sign up page

---
+++ src/services/UserService.js
@@ -0,0 +1,114 @@
+export default class UserService {
+
+    static myInstance = null;
+
+
+    static getInstance() {
+        if (UserService.myInstance == null) {
+            UserService.myInstance =
+                new UserService();
+        }
+        return this.myInstance;
+    }
+
+    //--------------------------------------------------------------------
+
+
+    createUser = (user) => {
+        let pathSplit = window.location.href.split("/");
+        let host = pathSplit[2];
+        let fetchHost;
+        if (host === "localhost:3000") {
+            fetchHost = "http://localhost:8080"
+        } else {
+            fetchHost = "https://wbdv-server-as4.herokuapp.com"
+        }
+
+        return fetch(`${fetchHost}/api/users`, {
+            method: 'POST',
+            body: JSON.stringify(user),
+            headers: {
+                'content-type': 'application/json'
+            }
+        })
+            .then(response => response.json());
+    };
+
+    //--------------------------------------------------------------------
+
+    deleteUser = (userId) => {
+        let pathSplit = window.location.href.split("/");
+        let host = pathSplit[2];
+        let fetchHost;
+        if (host === "localhost:3000") {
+            fetchHost = "http://localhost:8080"
+        } else {
+            fetchHost = "https://wbdv-server-as4.herokuapp.com"
+        }
+
+        return fetch(`${fetchHost}/api/users/${userId}`, {
+            method: 'DELETE'
+        })
+            .then(response => response.json())
+    };
+
+    //--------------------------------------------------------------------
+
+    findAllUsers = () => {
+
+        let pathSplit = window.location.href.split("/");
+        let host = pathSplit[2];
+        let fetchHost;
+        if (host === "localhost:3000") {
+            fetchHost = "http://localhost:8080"
+        } else {
+            fetchHost = "https://wbdv-server-as4.herokuapp.com"
+        }
+
+
+        return fetch(`${fetchHost}/api/users`)
+            .then(response => response.json());
+    };
+
+    //--------------------------------------------------------------------
+
+    findUserById = (userId) => {
+        let pathSplit = window.location.href.split("/");
+        let host = pathSplit[2];
+        let fetchHost;
+        if (host === "localhost:3000") {
+            fetchHost = "http://localhost:8080"
+        } else {
+            fetchHost = "https://wbdv-server-as4.herokuapp.com"
+        }
+
+        return fetch(`${fetchHost}/api/users/${userId}`)
+            .then(response => response.json());
+    };
+
+    //--------------------------------------------------------------------
+
+    updateUser = (user) => {
+
+        let pathSplit = window.location.href.split("/");
+        let host = pathSplit[2];
+        let fetchHost;
+        if (host === "localhost:3000") {
+            fetchHost = "http://localhost:8080"
+        } else {
+            fetchHost = "https://wbdv-server-as4.herokuapp.com"
+        }
+
+        let userId = user.id;
+
+        return fetch(`${fetchHost}/api/users/${userId}`, {
+            method: 'PUT',
+            body: JSON.stringify(user),
+            headers: {
+                'content-type': 'application/json'
+            }
+        })
+            .then(response => response.json())
+
+    };
+}
