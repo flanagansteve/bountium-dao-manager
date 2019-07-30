4fcc51a1222b42c70bdbefff440556358f376b3a
Steve Flanagan <flanagan.s@husky.neu.edu>
2019-07-30 13:55:04 -0400

    fixed card rendering in chrome

--- public/js/components/ProductCard.js
+++ public/js/components/ProductCard.js
@@ -6,7 +6,7 @@ var ProductCard = React.createClass({
 
   render : function() {
     return React.createElement("a", {href:"#", className:"link-unstyled"},
-      React.createElement("div", {className:"card d-flex", onClick:this.renderProduct},
+      React.createElement("div", {className:"card", onClick:this.renderProduct},
         React.createElement("img", {className:"card-img-top mx-auto d-block", alt:this.props.product.name, src:this.props.product.imageUrl}),
         React.createElement("div", {className:"card-body"},
           // TODO can we make the product name appear as a link w/o really being a link?
